bf1f3516a90b9e68885abf673c4583b1978fe34a
Daniel Jagoda <danielJagoda@o2.pl>
2024-03-10 06:02:34 +0100

    solved

---
+++ script.ts
@@ -0,0 +1,52 @@
+
+/*let array: number[] = [1,7,34,6,3]; 
+let min = array[0]
+let max = array[0]
+
+for(let i: number = 1; i<array.length; i++){
+
+    if(array[i] < min){
+        min = array[i]
+    }
+    if (array[i] > max) {
+        max = array[i];
+    }
+
+
+}
+console.log('min: ', min)
+console.log('max: ', max)
+*/
+
+/*
+
+let arrays: number[] = [0, 1];
+
+for(let i: number = 2; i < 10; i++) {
+    let nextFibonacci = arrays[i - 1] + arrays[i - 2];
+    arrays.push(nextFibonacci);
+}
+
+console.log(arrays);
+
+*/
+
+
+/*
+
+let arrays: number = 173;
+let bin: number[] = [];
+
+while (arrays >= 1) {
+    if (arrays % 2 === 0) {
+        bin.push(0);
+    } else {
+        bin.push(1);
+    }
+    
+    arrays = Math.floor(arrays / 2)
+
+}
+console.log(bin)
+
+*/
